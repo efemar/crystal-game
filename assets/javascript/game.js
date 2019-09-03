3a33789ac13a1cdfd83070fe12470e2d5443eef0
Carmen SanDiego <efemar82@gmail.com>
2019-09-02 20:04:35 -0700

    All initial files

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+
+
+// Game starts when the page is refreshed, a number is randomly generated
+//Each crystal has a new random number between 1 and 12
+//The computer generated number (goal) is randomly generated between 19-120
+
+
+$(".pic-crystals").on("click", function () {
+    alert ("the game has started");
+}
